92f6b41c950208a844d2a11c77a648daa4fde47f
Ivan Tomchev <ivantomchev@gmail.com>
2016-11-25 15:40:24 +0200

    Initial web api project created.

---
+++ Source/Server/TravelAndSave.Server.Api/Config/WebApiConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace TravelAndSave.Server.Api.Config
+{
+    using Newtonsoft.Json.Serialization;
+    using System.Web.Http;
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ Source/Server/TravelAndSave.Server.Api/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+using TravelAndSave.Server.Api.Config;
+
+[assembly: OwinStartup(typeof(TravelAndSave.Server.Api.Startup))]
+
+namespace TravelAndSave.Server.Api
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var httpConfiguration = new HttpConfiguration();
+            WebApiConfig.Register(httpConfiguration);
+
+            app.UseWebApi(httpConfiguration);
+        }
+    }
+}
