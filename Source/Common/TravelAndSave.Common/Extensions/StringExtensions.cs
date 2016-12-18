e86093fa9e80a70adac8b34018f978d431701af4
Ivan Tomchev <ivantomchev@gmail.com>
2016-12-18 09:48:47 +0200

    ReplaceLastOccurrence String extension added.

---
+++ Source/Common/TravelAndSave.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace TravelAndSave.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ReplaceLastOccurrence(this string source, string oldValue, string newValue)
+        {
+            int place = source.LastIndexOf(oldValue);
+
+            if (place == -1)
+            {
+                return string.Empty;
+            }
+
+            string result = source.Remove(place, oldValue.Length).Insert(place, newValue);
+
+            return result;
+        }
+    }
+}
