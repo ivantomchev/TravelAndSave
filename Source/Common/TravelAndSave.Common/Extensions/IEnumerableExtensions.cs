c27b731486f6592a5de38b059263b601d0743bea
Ivan Tomchev <ivantomchev@gmail.com>
2016-12-18 09:48:02 +0200

    DistinctBy and ForEach IEnumerable extensions added.

---
+++ Source/Common/TravelAndSave.Common/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace TravelAndSave.Common.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> action)
+        {
+            foreach (var item in collection)
+            {
+                action(item);
+            }
+        }
+    }
+}
