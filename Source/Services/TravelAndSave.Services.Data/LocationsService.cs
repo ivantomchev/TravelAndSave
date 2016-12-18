97938031c670f639193dca45f220b1dff95f8bc9
Ivan Tomchev <ivantomchev@gmail.com>
2016-12-18 11:03:25 +0200

    Initial LocationsService added.

---
+++ Source/Services/TravelAndSave.Services.Data/LocationsService.cs
@@ -0,0 +1,87 @@
+﻿namespace TravelAndSave.Services.Data
+{
+    using Common.Models;
+    using System.Collections.Generic;
+    using System.Linq;
+    using TravelAndSave.Data.Models;
+    using TravelAndSave.Data.Repositories;
+    using TravelAndSave.Data.Repositories.Base;
+
+    public class LocationsService
+    {
+        private readonly IDbRepository<Location> locationsRepository;
+
+        public LocationsService(IDbRepository<Location> themeRepository)
+        {
+            this.locationsRepository = themeRepository;
+        }
+
+        public LocationsService()
+            : this(new LocationsRepository())
+        {
+        }
+
+        public Result<Location> GetById(int id)
+        {
+            var location = this.locationsRepository.GetById(id);
+            if (location == null)
+            {
+                return Result.Fail<Location>(string.Format("Location with id {0} doesn't exist.", id), 404);
+            }
+
+            return Result.Ok(location);
+        }
+
+        public Result Add(Location location, bool saveChanges = true)
+        {
+            this.locationsRepository.Add(location);
+            if (saveChanges)
+            {
+                this.locationsRepository.SaveChanges();
+            }
+
+            return Result.Ok();
+        }
+
+        public Result Update(Location location, bool saveChanges = true)
+        {
+            var locationResult = this.GetById(location.Id);
+            if (locationResult.IsFailure)
+            {
+                return Result.Fail(locationResult);
+            }
+
+            this.locationsRepository.Update(location);
+            if (saveChanges)
+            {
+                this.locationsRepository.SaveChanges();
+            }
+
+            return Result.Ok();
+        }
+
+        public Result Detete(int id, bool saveChanges = true)
+        {
+            var locationResult = this.GetById(id);
+            if (locationResult.IsFailure)
+            {
+                return Result.Fail(locationResult);
+            }
+
+            this.locationsRepository.Delete(locationResult.Value);
+            if (saveChanges)
+            {
+                this.locationsRepository.SaveChanges();
+            }
+
+            return Result.Ok();
+        }
+
+        public Result<IEnumerable<Location>> GetAll()
+        {
+            var locations = this.locationsRepository.All().AsEnumerable();
+
+            return Result.Ok(locations);
+        }
+    }
+}
