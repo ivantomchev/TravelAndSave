82d08dd05a5519d9d9d7e03dd6a208afc09d7fc7
Ivan Tomchev <ivantomchev@gmail.com>
2017-03-07 09:59:33 +0200

    some test implementation added

--- Source/Server/TravelAndSave.Server.Authorization/Providers/AuthorizationServerProvider.cs
+++ Source/Server/TravelAndSave.Server.Authorization/Providers/AuthorizationServerProvider.cs
@@ -1,6 +1,9 @@
 ﻿namespace TravelAndSave.Server.Authorization.Providers
 {
+    using Microsoft.Owin.Security;
     using Microsoft.Owin.Security.OAuth;
+    using System.Collections.Generic;
+    using System.Security.Claims;
     using System.Threading.Tasks;
 
     public class AuthorizationServerProvider : OAuthAuthorizationServerProvider
@@ -13,12 +16,30 @@
 
         public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            var props = CreateProperties("FirstName", "LastName", "Administrator");
+            AuthenticationTicket ticket = new AuthenticationTicket(identity, props);
+            context.Validated(ticket);
+
             return base.GrantResourceOwnerCredentials(context);
         }
 
         public override Task GrantRefreshToken(OAuthGrantRefreshTokenContext context)
         {
             return base.GrantRefreshToken(context);
+        }
+
+        public static AuthenticationProperties CreateProperties(string firstName, string lastName, string role)
+        {
+            IDictionary<string, string> data = new Dictionary<string, string>
+            {
+                {"FirstName", firstName },
+                { "LastName", lastName },
+                {"Role", role }
+            };
+
+            return new AuthenticationProperties(data);
         }
     }
 }
--- Source/Server/TravelAndSave.Server.Authorization/Providers/RefreshTokenProvider.cs
+++ Source/Server/TravelAndSave.Server.Authorization/Providers/RefreshTokenProvider.cs
@@ -3,27 +3,42 @@
     using System;
     using System.Threading.Tasks;
     using Microsoft.Owin.Security.Infrastructure;
+    using System.Security.Claims;
+    using Microsoft.Owin.Security;
 
     public class RefreshTokenProvider : IAuthenticationTokenProvider
     {
+        private string inMemoryProtectedData;
+
         public void Create(AuthenticationTokenCreateContext context)
         {
-            throw new NotImplementedException();
         }
 
         public Task CreateAsync(AuthenticationTokenCreateContext context)
         {
-            throw new NotImplementedException();
+            ClaimsIdentity claimsIdentity = context.Ticket.Identity;
+            var refreshTokenProperties = new AuthenticationProperties(context.Ticket.Properties.Dictionary);
+            refreshTokenProperties.IssuedUtc = context.Ticket.Properties.IssuedUtc;
+            refreshTokenProperties.ExpiresUtc = DateTime.UtcNow.AddDays(10);
+
+            var refreshTokenTicket = new AuthenticationTicket(claimsIdentity, refreshTokenProperties);
+            this.inMemoryProtectedData = context.SerializeTicket();
+            context.SetToken("test refresh token");
+
+            return Task.FromResult<object>(null);
         }
 
         public void Receive(AuthenticationTokenReceiveContext context)
         {
-            throw new NotImplementedException();
         }
 
         public Task ReceiveAsync(AuthenticationTokenReceiveContext context)
         {
-            throw new NotImplementedException();
+            if (!string.IsNullOrEmpty(this.inMemoryProtectedData))
+            {
+                context.DeserializeTicket(this.inMemoryProtectedData);
+            }
+            return Task.FromResult<object>(null);
         }
     }
 }
